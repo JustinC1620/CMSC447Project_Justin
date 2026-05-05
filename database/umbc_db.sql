9d4efa0c0f58dbf1739607c1a3d2b2464c8726dd
JustinC1620 <jacollier1620@gmail.com>
2026-05-05 14:09:27 -0400

    exports

--- database/umbc_db.sql
+++ database/umbc_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2026 at 02:04 AM
+-- Generation Time: May 05, 2026 at 08:08 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
